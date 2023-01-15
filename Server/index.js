9eeec2823606e9f7b061bc62b594d0b591e9db4a
rafiqcoder <rafiqcoder@gmail.com>
2023-01-16 03:00:04 +0600

    added checkout and sslcommerz

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,14 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 const { MongoClient,ServerApiVersion,ObjectId } = require('mongodb');
-
+const SSLCommerzPayment = require('sslcommerz-lts')
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const store_id = 'webdc5f47477bc4df2'
+const store_passwd = 'webdc5f47477bc4df2@ssl'
+const is_live = false //true for live, false for sandbox
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sundialcluster.nmgilo7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri,{ useNewUrlParser: true,useUnifiedTopology: true,serverApi: ServerApiVersion.v1 });
@@ -37,6 +40,7 @@ async function run() {
     // const Categories = client.db('ResaleCycle').collection('categories');
     const BBQProducts = client.db('SundialDb').collection('BBQProducts');
     const CartDb = client.db('SundialDb').collection('CartProducts');
+    const OrdersDb = client.db('SundialDb').collection('OrdersDb');
     // const AdvertisedProducts = client.db('ResaleCycle').collection('advertised');
     // const Bookings = client.db('ResaleCycle').collection('bookings');
     // const ReportedItems = client.db('ResaleCycle').collection('reportedItems');
@@ -121,6 +125,56 @@ async function run() {
             const BBQproducts = await BBQProducts.findOne({ _id: ObjectId(id) })
 
             res.send(BBQproducts)
+        })
+
+
+
+        // SSlcommerz
+
+        app.post('/orderBbq',async (req,res) => {
+            const orderData = req.body;
+            // console.log(orderData.cartData.totalPrice);
+            // const result = await OrdersDb.insertOne(orderData);
+            // console.log(result);
+            // return res.send(result);
+            const data = {
+                total_amount: 100,
+                currency: 'BDT',
+                tran_id: 'REF123', // use unique tran_id for each api call
+                success_url: 'http://localhost:3030/success',
+                fail_url: 'http://localhost:3030/fail',
+                cancel_url: 'http://localhost:3030/cancel',
+                ipn_url: 'http://localhost:3030/ipn',
+                shipping_method: 'Courier',
+                product_name: 'Computer.',
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: '',
+                cus_email: 'customer@example.com',
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+            };
+            const sslcz = new SSLCommerzPayment(store_id,store_passwd,is_live)
+            sslcz.init(data).then(apiResponse => {
+                // Redirect the user to payment gateway
+                let GatewayPageURL = apiResponse.GatewayPageURL
+                res.send({ url: GatewayPageURL })
+
+            });
+
         })
 
     } finally {
