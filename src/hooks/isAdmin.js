6020fe78a67c593a3800f5d9769724a2c80c9cde
rafiqcoder <rafiqcoder@gmail.com>
2023-03-12 13:58:18 +0600

    creadentioal

--- src/hooks/isAdmin.js
+++ src/hooks/isAdmin.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react';
+import { useState } from 'react';
 import { logOut } from '../store/actions/authActions';
 import { Base_url } from '../store/utils';
 
@@ -7,49 +7,48 @@ const isAdmin = (email) => {
 
     console.log('email',email);
     const checkAdmin = async (email) => {
-       try {
-           if (email !== undefined) {
-               if (email !== null && email !== '' && email !== undefined && admin === false) {
-                   const res = await fetch(`${Base_url}/admin?email=${email}`,{
-                       method: 'POST',
-                       headers: {
-                           'Content-Type': 'application/json',
-                           'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                       },
-                       credentials: 'include'
-                   })
-                   const data = await res.json();
-                   console.log('data',data);
-                   if (data) {
-                       console.log('data',data);
-                       if (data.logout) {
-                           localStorage.removeItem('accessToken');
+        try {
+            if (email !== undefined) {
+                if (email !== null && email !== '' && email !== undefined && admin === false) {
+                    const res = await fetch(`${Base_url}/admin?email=${email}`,{
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    })
+                    const data = await res.json();
+                    console.log('data',data);
+                    if (data) {
+                        console.log('data',data);
+                        if (data.logout) {
+                            localStorage.removeItem('accessToken');
 
 
-                           logOut()
+                            logOut()
 
-                           return;
-                       }
+                            return;
+                        }
 
-                       if (data.accessToken) {
-                           console.log("accessToken",data.accessToken)
-                           localStorage.setItem('accessToken',data.accessToken);
-                       }
-                       if (data.admin) {
+                        if (data.accessToken) {
+                            console.log("accessToken",data.accessToken)
+                            localStorage.setItem('accessToken',data.accessToken);
+                        }
+                        if (data.admin) {
 
-                           setAdmin(true);
-                       }
-                   }
+                            setAdmin(true);
+                        }
+                    }
 
 
-               }
-           }
-       } catch (error) {
-        console.log(error);
-       }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
     checkAdmin(email);
-   
+
 
     return [admin]
 
