e09eb10d1a8283fff142544a37a87796c2ed14d7
rafiqcoder <rafiqcoder@gmail.com>
2023-03-15 16:34:06 +0600

    janaina

--- src/hooks/isAdmin.js
+++ src/hooks/isAdmin.js
@@ -15,10 +15,8 @@ const isAdmin = () => {
                         headers: {
                             'Content-Type': 'application/json',
                             'authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-                            'Access-Control-Allow-Origin': 'no-cors',
-                            'Access-Control-Allow-Credentials': true
-                        },
-                        credentials: 'include',
+                            'Access-Control-Allow-Origin': 'no-cors'
+                        }
                     })
                     const data = await res.json();
                     console.log('data',data);
